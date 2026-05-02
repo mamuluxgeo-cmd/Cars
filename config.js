4fa7da687c34887e142e8d188a5e4f1b9f1a8e95
mamuluxgeo-cmd <mamuluxgeo@gmail.com>
2026-05-02 17:03:53 +0400

    Update API_URL comment and maintain URL

--- config.js
+++ config.js
@@ -1,3 +1,2 @@
-// ჩასვი შენი Apps Script Web App URL აქ.
-// მაგ.: "https://script.google.com/macros/s/AKfycb.../exec"
+// Apps Script Web App URL
 window.API_URL = "https://script.google.com/macros/s/AKfycbyn2ra3cjGgEC2pJuPs1BXzc2HW15_14XbEKdwm0xJg-AkzpzaE15m6pkTqCw238pKGlQ/exec";
